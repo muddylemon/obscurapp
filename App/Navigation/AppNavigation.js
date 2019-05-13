e3313cb8745666c0d4350c815ee1a13f43d659fa
lck <lance.kidwell@asynchrony.com>
2019-05-13 14:29:24 -0500

    Yankee doodle

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import SelectedPhoto from '../Containers/SelectedPhoto'
+import HomeScreen from '../Containers/HomeScreen'
+import ViewerScreen from '../Containers/ViewerScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  SelectedPhoto: { screen: SelectedPhoto },
+  HomeScreen: { screen: HomeScreen },
+  ViewerScreen: { screen: ViewerScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
