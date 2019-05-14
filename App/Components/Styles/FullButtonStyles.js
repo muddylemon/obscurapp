971b0953368577025dc86cc7046fff52a7dae26a
lck <lance.kidwell@asynchrony.com>
2019-05-14 10:41:19 -0500

    full button

--- App/Components/Styles/FullButtonStyles.js
+++ App/Components/Styles/FullButtonStyles.js
@@ -4,8 +4,8 @@ import { Fonts, Colors } from '../../Themes/';
 export default StyleSheet.create({
   button: {
     marginVertical: 5,
-    borderTopColor: Colors.ember,
-    borderBottomColor: Colors.steel,
+    borderTopColor: Colors.charcoal,
+    borderBottomColor: Colors.charcoal,
     borderTopWidth: 1,
     borderBottomWidth: 1,
     backgroundColor: Colors.prince,
@@ -13,7 +13,7 @@ export default StyleSheet.create({
   buttonText: {
     margin: 18,
     textAlign: 'center',
-    color: Colors.snow,
+    color: Colors.silver,
     fontSize: Fonts.size.medium,
     fontFamily: Fonts.type.bold,
   },
