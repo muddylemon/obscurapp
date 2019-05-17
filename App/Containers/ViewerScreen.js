d980c12a3333222938283dd58cbfa3e89edc795b
lck <lance.kidwell@asynchrony.com>
2019-05-16 19:17:38 -0500

    gestures

--- App/Containers/ViewerScreen.js
+++ App/Containers/ViewerScreen.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react'
-import { Image, View, Dimensions } from 'react-native'
-import { connect } from 'react-redux'
-import { ImageSelectors } from '../Redux/ImageRedux'
-import styles from './Styles/ViewerScreenStyle'
+import React, { Component } from 'react';
+import { Image, View, Dimensions } from 'react-native';
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { ImageSelectors } from '../Redux/ImageRedux';
+import styles from './Styles/ViewerScreenStyle';
 
-import { RNCamera } from 'react-native-camera'
-import RoundedButton from '../Components/RoundedButton'
+import { RNCamera } from 'react-native-camera';
+import RoundedButton from '../Components/RoundedButton';
 
-const { width } = Dimensions.get('window')
+const { width, height } = Dimensions.get('window');
 
 class ViewerScreen extends Component {
+  state = {
+    opacity: 0.5,
+  };
   ref = cam => (this.camera = cam);
   goBack = () => {
-    this.props.navigation.navigate('LaunchScreen')
+    this.props.navigation.navigate('LaunchScreen');
   };
-  render () {
-    const { image } = this.props
+  onPanGestureEvent = ev => {
+    const opacity = 1 - ev.nativeEvent.absoluteY / height;
+    this.setState({ opacity });
+  };
+  onSingleTap = ev => {
+    console.log({ ev });
+  };
+  render() {
+    const { image } = this.props;
 
     return (
       <View style={styles.container}>
@@ -26,26 +40,34 @@ class ViewerScreen extends Component {
           style={styles.backgroundImage}
         >
           <View style={styles.closeButton}>
-            <RoundedButton text='x' onPress={this.goBack} />
+            <RoundedButton text="x" onPress={this.goBack} />
           </View>
-          <Image
-            style={{
-              ...styles.backgroundImage,
-              height: (width / image.width) * image.height,
-              width: width,
-              opacity: 0.5
-            }}
-            source={{ uri: `data:${image.mime};base64,${image.data}` }}
-          />
+          <TapGestureHandler onHandlerStateChange={this.onSingleTap}>
+            <PanGestureHandler
+              minDist={20}
+              onGestureEvent={this.onPanGestureEvent}
+            >
+              <Image
+                style={{
+                  ...styles.backgroundImage,
+                  height: (width / image.width) * image.height,
+                  width: width,
+                  opacity: this.state.opacity,
+                  resizeMode: 'contain',
+                }}
+                source={{ uri: `data:${image.mime};base64,${image.data}` }}
+              />
+            </PanGestureHandler>
+          </TapGestureHandler>
         </RNCamera>
       </View>
-    )
+    );
   }
 }
 
 export default connect(
   state => ({
-    image: ImageSelectors.getImage(state)
+    image: ImageSelectors.getImage(state),
   }),
   null
-)(ViewerScreen)
+)(ViewerScreen);
